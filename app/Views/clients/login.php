b17e1a9172ae2e614172c27f283c7af0d1fe7d75
arontsoaMendrika <mendrikaarontsoa@gmail.com>
2026-07-20 11:48:35 +0300

    11:48

--- app/Views/clients/login.php
+++ app/Views/clients/login.php
@@ -10,7 +10,7 @@
       <form method="post" action="<?= site_url('login') ?>">
         <?= csrf_field() ?>
         <label class="form-label">Numéro de téléphone</label>
-        <input type="text" name="numero" class="form-control mb-3" placeholder="0331234567" required>
+        <input type="text" name="numero" class="form-control mb-3" placeholder="0331234567" value="0331234567" required>
         <button class="btn btn-primary w-100">Se connecter</button>
       </form>
       <a href="<?= site_url('admin') ?>" class="btn btn-outline-secondary w-100 mt-2">Espace Admin</a>
