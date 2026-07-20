7c09303a598a730cd2fe14cff68b1966c91d61cb
mandresy <mandresy128@gmail.com>
2026-07-20 16:50:53 +0300

    16:50

--- app/Controllers/Admin/Operateurs.php
+++ app/Controllers/Admin/Operateurs.php
@@ -49,16 +49,4 @@ class Operateurs extends BaseController
         \Config\Database::connect()->table('prefixe')->delete(['id' => $id]);
         return redirect()->back()->with('message', 'Préfixe supprimé.');
     }
-
-    public function updateOperateur($id)
-    {
-    $operateurModel = new OperateurModel();
-    $taux = $this->request->getPost('commission_pct'); 
-
-    $operateurModel->update($id, [
-        'commission_pct' => $taux
-    ]);
-
-    return redirect()->back()->with('message', 'Commission mise à jour.');
-    }
 }
